30733868e058d6d103966db9d3e81e68d3b03d13
Karan vishwakarma <150724533+Karan09542@users.noreply.github.com>
2025-08-11 16:10:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function isSameType(value1, value2) {
   //your js code here
 	value1.trim();value2.trim();
 	if(value1==="NaN" && value2==="NaN") return true;
-	else if((value1 !== "NaN" || value2 !== "NaN"){
-		if(typeof parseFloat(value1) !== number || typeof parseFloat(value2) !== number {
-			return false
+	else if((value1 !== "NaN" && value2 !== "NaN"){
+		if(typeof parseFloat(value1) !== number && typeof parseFloat(value2) !== number) {
+			return true
 		}
 		else return true
 	}
