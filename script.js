ddefa77b8d5984001afffb0615847fab91eeb929
Karan vishwakarma <150724533+Karan09542@users.noreply.github.com>
2025-08-11 17:14:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 function isSameType(value1, value2) {
   //your js code here
-	value1.trim();value2.trim();
-	if(value1==="NaN" && value2==="NaN") return true;
-	else if(value1 !== "NaN" && value2 !== "NaN"){
-		if(typeof parseFloat(value1) !== "number" && typeof parseFloat(value2) !== "number") {
-			return true
-		}
-		else return true
-	}
-	else return false
+		value1 = value1.trim();
+	value2 = value2.trim();
+	
+	const num1 = parseFloat(value1)
+	const num2 = parseFloat(value2)
+	const isNum1NaN = isNaN(num1)
+	const isNum2NaN = isNaN(num2)
+	
+	if(isNum1NaN && isNum2NaN) return true;
+	if(isNum1NaN || isNum2NaN) return false;
+	
+	return typeof num1 === typeof num2;
 }
 
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
+
+
